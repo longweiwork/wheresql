8aa2d1ba9a7cf9263ebfe7884729ef6f065ad354
longweiwork <32833794+longweiwork@users.noreply.github.com>
2021-07-22 15:34:44 +0800

    feat: 优化import

--- src/main/java/cn/lw/wheresql/operator/OperatorFactory.java
+++ src/main/java/cn/lw/wheresql/operator/OperatorFactory.java
@@ -1,9 +1,6 @@
 package cn.lw.wheresql.operator;
 
-import com.sun.tools.javac.code.Attribute;
-
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 import java.net.JarURLConnection;
 import java.net.URL;
